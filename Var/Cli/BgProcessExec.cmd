428dfa5703ebf99e7c25c876579afd34268070e4
Tom Flídr <tomflidr@gmail.com>
2021-01-17 11:28:39 +0100

    removed temp files

--- Var/Cli/BgProcessExec.cmd
+++
@@ -1,7 +0,0 @@
-@set id_bg_process=%1
-
-:: @set COMPUTERNAME=TOM
-@set bin_dir=%cd%
-@set app_root_dir=%bin_dir:\=/%/../..
-
-@Fork.exe php.exe -d max_execution_time=0 -d memory_limit=-1 "%app_root_dir%/index.php" controller=bg-processes/dispatcher action=execute id_bg_process=%id_bg_process%
